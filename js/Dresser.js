ae500cc1ad7084a191b7fd3d20773db8b36d7cbf
aaqidmasoodi <aaqidmasoodi@icloud.com>
2022-11-22 21:35:04 +0530

    added javascript logic for storage of model data

--- js/Dresser.js
+++ js/Dresser.js
@@ -11,6 +11,48 @@ clothingItemBtns.forEach((clothingBtn) => {
 
 
 
+let resetButton = document.getElementById("resetBtn")
+
+resetButton.addEventListener('click', () => {
+    window.location.replace("http://127.0.0.1:5500/LandingPage.html");
+    localStorage.clear()
+})
+
+
+
+// ---------------------------------------------------
+const modelInfo = JSON.parse(localStorage.getItem("modelInfo"));
+// ------ get name tags
+const modelNameTag = document.getElementById("model-name-tag");
+const modelAgeTag = document.getElementById("model-age-tag");
+const modelGenderTag = document.getElementById("model-gender-tag");
+const modelWeightTag = document.getElementById("model-weight-tag");
+const modelHeightTag = document.getElementById("model-height-tag");
+const modelBirthPlaceTag = document.getElementById("model-birthplace-tag");
+const modelEmotionTag = document.getElementById("model-emotions-tag");
+const modelHapinessTag = document.getElementById("model-happiness-tag");
+const modelCalmnessTag = document.getElementById("model-calmness-tag");
+const modelBraveryTag = document.getElementById("model-bravery-tag");
+const modelOrderlyTag = document.getElementById("model-oderly-tag");
+// ------ end get name tags
+
+modelNameTag.innerText = modelInfo.modelName;
+modelAgeTag.innerText = modelInfo.modelAge;
+modelGenderTag.innerText = modelInfo.modelGender;
+modelWeightTag.innerText = modelInfo.modelWeight;
+modelHeightTag.innerText = modelInfo.modelHeight;
+modelBirthPlaceTag.innerText = modelInfo.modelBirthPlace;
+modelEmotionTag.innerText = modelInfo.modelEmotion;
+modelHapinessTag.innerText = modelInfo.modelHapiness;
+modelCalmnessTag.innerText = modelInfo.modelCalmness;
+modelBraveryTag.innerText = modelInfo.modelBravery;
+modelOrderlyTag.innerText = modelInfo.modelOrderly;
+
+
+
+
+// ---------------------------------------------------
+
 let changeOutfitBtn = document.getElementById('otfts-btn')
 let outfitOverlay = document.getElementById("outfit-overlay")
 let outfitIndex = 1
--- js/onBoard.js
+++ js/onBoard.js
@@ -1,1 +1,40 @@
-const playerName = document.getElementById()
+const modelName = document.getElementById("name");
+const modelAge = document.getElementById("age");
+const modelGender = document.getElementById("gender");
+const modelWeight = document.getElementById("weight");
+const modelHeight = document.getElementById("height");
+const modelBirthPlace = document.getElementById("birthplace");
+// ------
+const modelEmotion = document.getElementById("emotional");
+const modelHapiness = document.getElementById("happiness");
+const modelCalmness = document.getElementById("calm");
+const modelBravery = document.getElementById("braveness");
+const modelOrderly = document.getElementById("orderly");
+
+
+
+const saveButton = document.getElementById("savebtn");
+
+
+saveButton.addEventListener('click', () => {
+
+    modelInfo = {
+        "modelName": modelName.value,
+        "modelAge": modelAge.value,
+        "modelGender": modelGender.value,
+        "modelWeight": modelWeight.value,
+        "modelHeight": modelHeight.value,
+        "modelBirthPlace": modelBirthPlace.value,
+        "modelEmotion": modelEmotion.value,
+        "modelHapiness": modelHapiness.value,
+        "modelCalmness": modelCalmness.value,
+        "modelBravery": modelBravery.value,
+        "modelOrderly": modelOrderly.value,
+
+    }
+
+    localStorage.setItem("modelInfo", JSON.stringify(modelInfo));
+
+    window.location.replace("http://127.0.0.1:5500/index.html");
+
+})
